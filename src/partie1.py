5cad931a69e8e4bac4ec64ab3f8248d3af7f5b6e
Esther <esther.choi@etu.sorbonne-universite.fr>
2020-11-21 19:37:15 +0100

    mini optimisations ColoriagePossibleRec2 et ColoreLig/Col

--- src/partie1.py
+++ src/partie1.py
@@ -57,17 +57,18 @@ def ColoriagePossibleRec2(V,s,j,l,T):
     if T[j][l] != VIDE: #si on a déjà calculé la valeur recherchée
         return T[j][l]
     elif l == 0: #cas de base 1
-        T[j][l] = TestVal(V,0,j,NOIR)   #test s'il y a une case noire avant (i,j), auquel cas T(j,l)=faux puisqu'on est pas censé avoir de bloc noir du tout
+        T[j][l] = TestVal(V,0,j,NOIR)   #teste s'il y a une case noire avant (i,j), auquel cas T(j,l)=faux puisqu'on est pas censé avoir de bloc noir du tout
         return T[j][l]
     elif (l == 1) and (j == s[0]-1): #cas de base de 2b
-        T[j][l] = TestVal(V,0,j,BLANC)  #test s'il y a une case blanche avant (i,j), auquel cas T(j,l)=faux puisque les j+1 premières cases contiennet le bloc 1
+        T[j][l] = TestVal(V,0,j,BLANC)  #test s'il y a une case blanche avant (i,j), auquel cas T(j,l)=faux puisque les j+1 premières cases contiennent le bloc 1
         return T[j][l] 
     elif (j < 0) or (l < 0):    #je le met car en python liste[-i] donne le i-ème élément de liste en partant de la fin, et nous on veut pas ça
         return False
     elif j <= s[l-1]-1: #cas de base 2a et 2b pour l!=1
         T[j][l] = False
         return False
-#A partir de là, on est dans le cas 2c j > s[l-1]-1
+    
+    #A partir de là, on est dans le cas 2c j > s[l-1]-1
 
     #si (i,j) est blanche
     elif V[j] == NOIR:   #on ne peut pas avoir (i,j) noire
@@ -75,23 +76,19 @@ def ColoriagePossibleRec2(V,s,j,l,T):
     else:   #il faut tester si les l premiers blocs rentrent dans les j premières cases
         b1 = ColoriagePossibleRec2(V,s,j-1,l,T) 
     
-    #si (i,j) est noire, alors (i,j) contient la dernière case du bloc l
-    if not TestVal(V,j-s[l-1]+1,j,BLANC):   #teste s'il y a une case blanche entre (i,j-(s[l-1]-1)) et (i,j), c'est-à-dire tout le long du bloc l
-        b2 = False  #car il ne doit pas y avoir de case blanche entre j-(s[l-1]-1) et j (puisqu'on est dans le bloc noir l)
-    elif V[j-s[l-1]] == NOIR:   #si le test est vérifié, cela signifie que le bloc l se termine à la case (i,j-1) donc (i,j) ne pourrait être noire
-        b2 = False
-    else:   #il faut tester si les l-1 premiers blocs rentrent dans les j-(s[l-1]1) premières cases
-        b2 = ColoriagePossibleRec2(V,s,j-s[l-1]-1,l-1,T)
-    
-    #si on ne sait pas la couleur de (i,j), alors on a calculé b1 et b2
+    if b1:  #si b1 = True
+        T[j][l] = True  #car la formule de récurrence de 2c s'écrit avec un 'ou'
+        return True
+    else:
 
-    # if b1 == True:      # en version if-else c'est ça non ? pourquoi ça marche pas ?
-    #     T[j][l] = True  # dans tous les cas, la version b1 or b2 est mieux car c'est la relation de récurrence de 2c
-    # elif b2 == True:
-    #     T[j][l] == True
-    # else:
-    #     T[j][l] = False
-    # # print(T[j][l])
+        #si (i,j) est noire, alors (i,j) contient la dernière case du bloc l
+        if not TestVal(V,j-s[l-1]+1,j,BLANC):   #teste s'il y a une case blanche entre (i,j-(s[l-1]-1)) et (i,j), c'est-à-dire tout le long du bloc l
+            b2 = False  #car il ne doit pas y avoir de case blanche entre j-(s[l-1]-1) et j (puisqu'on est dans le bloc noir l)
+        elif V[j-s[l-1]] == NOIR:   #si le test est vérifié, cela signifie que le bloc l se termine à la case (i,j-1) donc (i,j) ne pourrait être noire
+            b2 = False
+        else:   #il faut tester si les l-1 premiers blocs rentrent dans les j-(s[l-1]1) premières cases
+            b2 = ColoriagePossibleRec2(V,s,j-s[l-1]-1,l-1,T)
+
     T[j][l] = (b1 or b2)
     return T[j][l]
         
@@ -150,29 +147,26 @@ def ColoreLig(G,i,nouveaux): #on veut donc colorier V = G[0][i] avec la séquenc
     j = M-1
     l = len(s)
     T = init_matrice(M,l+1,0)
-    if not(ColoriagePossibleRec2(V,s,j,l,T)): #s'il n'y a pas de coloriage possible alors ce puzzle n'a pas de sol
-        return (False,G)
-    else:
-        for k in range(0,M):#on parcourt les M cases de la i-eme ligne
-            if V[k]==VIDE: #si on a pas encore colorié cette case
-                T = init_matrice(M,l+1,0)
-                V[k]=BLANC
-                caseb = ColoriagePossibleRec2(V,s,j,l,T) #on test si la case peut etre coloriée en blanc
-                T = init_matrice(M,l+1,0)
-                V[k]=NOIR
-                casen = ColoriagePossibleRec2(V,s,j,l,T) #on test si la case peut etre coloriée en noir
-                if(caseb and casen): #si la case peut etre coloriee en noir ou blanc on ne peut rien en conclure donc on remet V[k] a VIDE
-                    V[k] = VIDE 
-                elif (caseb and not(casen)): #si la case peut etre blanche mais ne peut etre noire alors on la colorie en blanc
-                    V[k] = BLANC
-                    G[0][i][k] = BLANC
-                    nouveaux.append(k)
-                elif (casen and not(caseb)): #si la case peut etre noire mais ne peut etre blanche alors on la colorie en noir
-                    V[k] = NOIR
-                    G[0][i][k] = NOIR
-                    nouveaux.append(k)
-                elif (not(caseb) and not(casen)): #si la case ne peut etre coloriée alors le puzzle n'a pas de solution
-                    return(False,G)
+    for k in range(0,M):#on parcourt les M cases de la i-eme ligne
+        if V[k]==VIDE: #si on a pas encore colorié cette case
+            T = init_matrice(M,l+1,0)
+            V[k]=BLANC
+            caseb = ColoriagePossibleRec2(V,s,j,l,T) #on test si la case peut etre coloriée en blanc
+            T = init_matrice(M,l+1,0)
+            V[k]=NOIR
+            casen = ColoriagePossibleRec2(V,s,j,l,T) #on test si la case peut etre coloriée en noir
+            if(caseb and casen): #si la case peut etre coloriee en noir ou blanc on ne peut rien en conclure donc on remet V[k] a VIDE
+                V[k] = VIDE 
+            elif (caseb and not(casen)): #si la case peut etre blanche mais ne peut etre noire alors on la colorie en blanc
+                V[k] = BLANC
+                G[0][i][k] = BLANC
+                nouveaux.append(k)
+            elif (casen and not(caseb)): #si la case peut etre noire mais ne peut etre blanche alors on la colorie en noir
+                V[k] = NOIR
+                G[0][i][k] = NOIR
+                nouveaux.append(k)
+            elif (not(caseb) and not(casen)): #si la case ne peut etre coloriée alors le puzzle n'a pas de solution
+                return(False,G)
     return (True,G)
 
 
@@ -192,34 +186,30 @@ def ColoreCol(G,j,nouveaux):
     i = N-1
     l = len(s)
     T = init_matrice(N,l+1,0)
-    if not(ColoriagePossibleRec2(V,s,i,l,T)):
-        return (False,G)
-    else:
-        for k in range(0,N):
-            if V[k]==VIDE:
-                T = init_matrice(N,l+1,0)
-                V[k]=BLANC
-                caseb = ColoriagePossibleRec2(V,s,i,l,T)
-                T = init_matrice(N,l+1,0)
-                V[k]=NOIR
-                casen = ColoriagePossibleRec2(V,s,i,l,T)
-                if (casen and caseb):
-                    V[k] = VIDE
-                elif (caseb and not(casen)):
-                    V[k] = BLANC
-                    G[0][k][j] = BLANC
-                    nouveaux.append(k)
-                elif (casen and not(caseb)):
-                    V[k] = NOIR
-                    G[0][k][j] = NOIR
-                    nouveaux.append(k)
-                elif (not(caseb) and not(casen)):
-                    return(False,G)
+    for k in range(0,N):
+        if V[k]==VIDE:
+            T = init_matrice(N,l+1,0)
+            V[k]=BLANC
+            caseb = ColoriagePossibleRec2(V,s,i,l,T)
+            T = init_matrice(N,l+1,0)
+            V[k]=NOIR
+            casen = ColoriagePossibleRec2(V,s,i,l,T)
+            if (casen and caseb):
+                V[k] = VIDE
+            elif (caseb and not(casen)):
+                V[k] = BLANC
+                G[0][k][j] = BLANC
+                nouveaux.append(k)
+            elif (casen and not(caseb)):
+                V[k] = NOIR
+                G[0][k][j] = NOIR
+                nouveaux.append(k)
+            elif (not(caseb) and not(casen)):
+                return(False,G)
     return (True,G)
 
 
-# Algothme de coloration
-# TODO
+# Question 9 : Algothme de coloration
 def coloration(A):
     G = copy.deepcopy(A)   #copie de la grille
     N = len(G[0])  #nombre de lignes
@@ -264,4 +254,3 @@ def propagation(filepath):
         print("L'instance n'a pas de solution.")
     else:
         print("On ne peut pas conclure.")
-    
